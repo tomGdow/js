17c571e00d061baf4bd81f3a7d0d50363a24ddf3
Thomas Dowling <thomasgdowling@gmail.com>
2025-05-08 10:21:49 +0100

    create and populate exam_objectives.html

--- js/constants_variables.js
+++ js/constants_variables.js
@@ -6,10 +6,10 @@ baseFileName = getBaseFileName(baseFileName);
 // }
 
 //Add New FileName(s)
-let pageArr = ["index", "template_first_page", "template_second_page"];
+let pageArr = ["index", "exam_objectives", "template_second_page"];
 pageArr.push("search"); // 'search' should be the last term in pageArr
 
-let pageTitleArr = ["ckr-index", "ckr-template", "ckr-template-two", "ckr-search"];
+let pageTitleArr = ["js-index", "js-exam-objectives", "ckr-template-two", "ckr-search"];
 
 // Add banner third part
 const bannerThirdPart = "bannerPart3";
