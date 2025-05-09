9a1f13763960aaadd0d2df51643e3b6c6c3defc5
Thomas Dowling <thomasgdowling@gmail.com>
2025-05-09 23:45:38 +0100

    update constants_variables to reflect creation of increment_decrement.html

--- js/constants_variables.js
+++ js/constants_variables.js
@@ -6,16 +6,16 @@ baseFileName = getBaseFileName(baseFileName);
 // }
 
 //Add New FileName(s)
-let pageArr = ["index", "exam_objectives", "template_second_page"];
+let pageArr = ["index", "exam_objectives", "assignment",  "increment_decrement"];
 pageArr.push("search"); // 'search' should be the last term in pageArr
 
-let pageTitleArr = ["js-index", "js-exam-objectives", "ckr-template-two", "ckr-search"];
+let pageTitleArr = ["js-index", "js-exam-objectives", "assignment","incr-decr", "js-search"];
 
 // Add banner third part
-const bannerThirdPart = "bannerPart3";
+const bannerThirdPart = "js";
 
 // Add (meta) keyword list
-const myKeywordList = "my, keyword, list";
+const myKeywordList = "javascript, IT, specialist";
 
 // Add (meta) description
-const myDescription = "my site description";
+const myDescription = "Notes for the IT Specialist in JavaScript Course at ecollege";
