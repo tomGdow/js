b10c7167321dbd9a0725a81267b4d8e1167fb2ab
Thomas Dowling <thomasgdowling@gmail.com>
2025-05-11 15:19:51 +0100

    update constants_variable.html with data for plus.html

--- js/constants_variables.js
+++ js/constants_variables.js
@@ -6,10 +6,10 @@ baseFileName = getBaseFileName(baseFileName);
 // }
 
 //Add New FileName(s)
-let pageArr = ["index", "exam_objectives", "assignment",  "increment_decrement","domain_one_questions_part_one"];
+let pageArr = ["index", "exam_objectives", "assignment", "increment_decrement", "plus", "domain_one_questions_part_one"];
 pageArr.push("search"); // 'search' should be the last term in pageArr
 
-let pageTitleArr = ["js-index", "js-exam-objectives", "assignment","incr-decr","d1Q", "js-search"];
+let pageTitleArr = ["js-index", "js-exam-objectives", "assignment", "incr-decr", "plus", "d1Q", "js-search"];
 
 // Add banner third part
 const bannerThirdPart = "js";
