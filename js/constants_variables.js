4b9f20d7f3f54452dcd3e0e7be86f3374d39b67a
Thomas Dowling <thomasgdowling@gmail.com>
2025-05-14 06:59:22 +0100

    create, hook-up and populate subtraction_assignment.html

--- js/constants_variables.js
+++ js/constants_variables.js
@@ -6,10 +6,10 @@ baseFileName = getBaseFileName(baseFileName);
 // }
 
 //Add New FileName(s)
-let pageArr = ["index", "exam_objectives", "assignment", "increment_decrement", "plus", "minus", "multiply","divide", "power", "modulus", "coercion", "domain_one_questions_part_one"];
+let pageArr = ["index", "exam_objectives", "assignment", "increment_decrement", "plus", "minus", "multiply", "divide", "power", "modulus", "addition_assignment", "subtraction_assignment", "coercion", "domain_one_questions_part_one"];
 pageArr.push("search"); // 'search' should be the last term in pageArr
 
-let pageTitleArr = ["js-index", "js-exam-objectives", "assignment", "incr-decr", "plus","minus","multiply","divide","power","mod",  "coercion","d1Q", "js-search"];
+let pageTitleArr = ["js-index", "js-exam-objectives", "assignment", "incr-decr", "plus", "minus", "multiply", "divide", "power", "mod", "+assignment", "-assignment", "coercion", "d1Q", "js-search"];
 
 // Add banner third part
 const bannerThirdPart = "js";
