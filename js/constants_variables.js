869960a14bfdd932fc553d2f1b72fade25e778fc
Thomas Dowling <thomasgdowling@gmail.com>
2025-05-24 10:46:36 +0100

    create and hook-up domain_two_questions_1-10.html from questions_template.html

--- js/constants_variables.js
+++ js/constants_variables.js
@@ -7,11 +7,11 @@ baseFileName = getBaseFileName(baseFileName);
 
 //Add New FileName(s)
 let pageArr = ["index", "exam_objectives", "assignment", "increment_decrement", "plus", "minus", "multiply", "divide", "power", "modulus", "addition_assignment", "subtraction_assignment", "multiplication_assignment", "division_assignment", "remainder_assignment", "exponentiation_assignment", "coercion", "comments", "indentation", "naming_conventions", "noscript", "const", "let_var", "temporal_dead_zone", "domain_one_questions_part_one",
-    "domain_one_questions_part_two", "errors_quiz", "reserved_keywords", "debugger", "console_log", "script", "try_catch", "bom", "screen_browser_height"];
+    "domain_one_questions_part_two", "errors_quiz", "reserved_keywords", "debugger", "console_log", "script", "try_catch", "bom", "screen_browser_height", "domain_two_questions_1-10"];
 
 pageArr.push("search"); // 'search' should be the last term in pageArr
 
-let pageTitleArr = ["js-index", "js-exam-objectives", "assignment", "incr-decr", "plus", "minus", "multiply", "divide", "power", "mod", "+assignment", "-assignment", "*assignment", "/assignment", "%assignment", "**exponentiation", "coercion", "comments", "indentation", "naming", "noscript", "const", "let_var", "TDZ", "d1Q-a", "d1Q-b", "err_quiz", "reserved", "debugger", "console", "script", "try catch", "bom", "sb-height", "js-search"];
+let pageTitleArr = ["js-index", "js-exam-objectives", "assignment", "incr-decr", "plus", "minus", "multiply", "divide", "power", "mod", "+assignment", "-assignment", "*assignment", "/assignment", "%assignment", "**exponentiation", "coercion", "comments", "indentation", "naming", "noscript", "const", "let_var", "TDZ", "d1Q-a", "d1Q-b", "err_quiz", "reserved", "debugger", "console", "script", "try catch", "bom", "sb-height", "d2 1-10", "js-search"];
 
 // Add banner third part
 const bannerThirdPart = "js";
